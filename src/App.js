018a3ae1c3d5ca7ebdce57f7e6b3a711dd492b09
vrato-sabo <vrato.sabo@gmail.com>
2021-12-03 08:33:48 +0100

    title change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import { useEffect } from 'react';
 import Header from './components/Header';
 import Movies from './components/Movies';
 
 function App() {
+  useEffect(() => {
+    document.title = 'MovieDatabase';
+  }, []);
   return (
     <div>
       <Header />
-
       <Movies />
     </div>
   );
